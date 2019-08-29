efc144b38220fd6fca6b5a3804ef1b458cdba289
GuillaumeAichhorn <aichhorn.guillaume@gmail.com>
2019-08-29 16:19:10 +0200

    Shows the project members list only to the project owner

--- packages/client/src/scenes/Project/components/SideBar/SideBarComponent.tsx
+++ packages/client/src/scenes/Project/components/SideBar/SideBarComponent.tsx
@@ -18,7 +18,7 @@ import UserAvatar from 'components/UserAvatar';
 import VisibilityChip from 'components/VisibilityChip';
 import * as React from 'react';
 import { AsyncAction } from 'types/ActionTypes';
-import { isOwner } from 'utils/ProjectUtils';
+import { isOwner, isMember } from 'utils/ProjectUtils';
 
 import ShareCredentials from 'components/ShareCredentials';
 
@@ -154,7 +154,7 @@ export default withStyles(styles)(withI18n()(({
                 name={project.shareName}
                 password={project.sharePassword}
               />
-              
+
               {t('project.share.dialog.description')}
               <a
                 href={`/shares/${project.id}?p=${project.sharePassword}`}
@@ -181,18 +181,19 @@ export default withStyles(styles)(withI18n()(({
           </ButtonProgress>
         </div>
       } */}
-      <List
-        dense={true}
-        className={classes.list}
-        subheader={
+       {(user && (isMember(project, user) || isOwner(project, user)))  &&
+        <List
+          dense={true}
+          className={classes.list}
+          subheader={
           <ListSubheader
             className={classes.listHeader}
           >
             {t('project.members', { count: members.size })}
           </ListSubheader>
         }
-      >
-        {Array.from(members).map((member: Member) => (
+        >
+          {Array.from(members).map((member: Member) => (
           <ListItem key={member.id} className={classes.listItem}>
             <ListItemAvatar>
               <UserAvatar user={member} />
@@ -203,7 +204,7 @@ export default withStyles(styles)(withI18n()(({
             />
           </ListItem>
         ))}
-      </List>
+        </List>}
       {(user && isOwner(project, user)) &&
         <div className={classes.button}>
           <ButtonProgress
