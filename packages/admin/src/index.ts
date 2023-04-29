a9bcfec62c5f8689755d27877b5630c2d5c006d6
Younes <younes.benaomar@gmail.com>
2023-04-29 11:31:13 +0100

    add cors

--- packages/admin/src/index.ts
+++ packages/admin/src/index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
 import path from "path";
 import * as url from 'url'
 
@@ -7,16 +8,35 @@ import getAdminRouter from "./server.js";
 
 const start = async () => {
   const app = express();
+  app.use(cors());
+
+  // Define the CORS middleware function
+  const corsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    // Set the CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Call the next middleware function in the chain
+    next();
+  };
+
+  // Add the CORS middleware function to the application
+  app.use(corsMiddleware);
+
 
   const adminRouter = await getAdminRouter({
     rootPath: "/admin"
   });
 
-  app.use('/admin', adminRouter);
+
+  app.use('/', adminRouter);
 
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
   app.use("/admin", express.static(path.join(__dirname, "../public")));
+
+  app.get("/*", (_, res) => res.status(404).send("page not found"));
 
   app.listen(3000, () => {
     console.log(
