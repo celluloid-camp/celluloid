a8a636a686cf19836ad55d3668a5df0318ab31eb
Younes <younes.benaomar@gmail.com>
2023-05-02 15:22:42 +0100

    Fix bcrypt module (#205)
    * fix module import and types
    
    * fix docker cmd
    
    * add curl for healthcheck
    
    * add database and config package
    
    * add catch all router for admin
    
    * fix static path
    
    * add cors
    
    * add logs
    
    * move i18next folder

--- packages/prisma/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from './generated/index.js'
-import { Context } from "./index.d.js";
-
-const prisma = new PrismaClient();
-
-export const createContext = async (): Promise<Context> => ({ prisma });
-
-export * from './generated/runtime/index.js'
