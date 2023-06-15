a9bd9e6a0aeecacb71b40829e09899f1a0c7bb96
Younes <younes.benaomar@gmail.com>
2023-06-15 12:41:33 +0100

    fix avatar style

--- packages/client/src/components/UserAvatar.tsx
+++ packages/client/src/components/UserAvatar.tsx
@@ -10,25 +10,27 @@ type UserAvatarProps = AvatarProps & {
 };
 
 export const UserAvatar: React.FC<UserAvatarProps> = React.memo(
-  ({ userId, username, small, sx, ...props }) => (
-    <Avatar
-      sx={[
-        small
-          ? {
-              height: 24,
-              width: 24,
-              backgroundColor: getUserColor(userId),
-            }
-          : {
-              height: 40,
-              width: 40,
-              backgroundColor: getUserColor(userId),
-            },
-        sx,
-      ]}
-      {...props}
-    >
-      {getUserInitials(username)}
-    </Avatar>
-  )
+  ({ userId, username, small, sx, ...props }) => {
+    const customStyle = small
+      ? {
+          height: 24,
+          width: 24,
+          backgroundColor: getUserColor(userId),
+        }
+      : {
+          height: 40,
+          width: 40,
+          backgroundColor: getUserColor(userId),
+        };
+
+    const styles = {
+      ...sx,
+      ...customStyle,
+    };
+    return (
+      <Avatar sx={styles} {...props}>
+        {getUserInitials(username)}
+      </Avatar>
+    );
+  }
 );
