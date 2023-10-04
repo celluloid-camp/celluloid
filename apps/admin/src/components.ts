f96fb8bf64205f0a56458cc2882897df62d60fd9
Younes <younes.benaomar@gmail.com>
2023-10-04 18:49:01 +0100

    fix admin build

--- apps/admin/src/components.ts
+++ apps/admin/src/components.ts
@@ -3,7 +3,7 @@ import { ComponentLoader } from 'adminjs'
 const componentLoader = new ComponentLoader()
 
 const Components = {
-  // MyInput: componentLoader.add('MyInput', './my-input'),
+  MyInput: componentLoader.add('MyInput', './my-input'),
   // other custom components
 }
 
--- apps/admin/tsup.config.ts
+++ apps/admin/tsup.config.ts
@@ -5,8 +5,9 @@ const isProduction = process.env.NODE_ENV === "production";
 export default defineConfig({
   clean: true,
   dts: false,
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  entry: ["src/index.ts", "src/my-input.tsx"],
+  format: ["esm"],
+  target: "esnext",
   minify: isProduction,
   sourcemap: false,
 });
