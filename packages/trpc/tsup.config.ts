19565e24a2d0310d84b0b29d27682aad4d5c311f
Younes <younes.benaomar@gmail.com>
2023-10-04 19:32:03 +0100

    clean build tools

--- packages/trpc/tsup.config.ts
+++ packages/trpc/tsup.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
+  target: "esnext",
+  format: ["esm"],
   minify: isProduction,
   sourcemap: true,
 });
