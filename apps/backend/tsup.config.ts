c28d05b822c70bf85c04307ce1bfc60367648576
Younes <younes.benaomar@gmail.com>
2023-10-04 15:50:27 +0100

    fix backend build

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -4,9 +4,9 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   clean: true,
-  dts: true,
+  dts: false,
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   minify: isProduction,
-  sourcemap: true,
+  sourcemap: false,
 });
