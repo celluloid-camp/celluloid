fdc11cee713e18d0ff7e3648a8a00cda4e3afb23
Younes <younes.benaomar@gmail.com>
2023-10-09 18:00:42 +0100

    fix shared layout user refresh

--- apps/frontend/src/components/SharedLayout.tsx
+++ apps/frontend/src/components/SharedLayout.tsx
@@ -1,12 +1,44 @@
 import * as React from "react";
+import { useEffect } from "react";
+import { connect } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { AnyAction, Dispatch } from "redux";
+
+import {
+  doLogoutThunk,
+  fetchCurrentUserThunk,
+} from "~actions/Signin/UserActions";
 
 import { AppBarMenu } from "./AppBarMenu";
 
-export const SharedLayout: React.FC = () => {
+type Props = React.PropsWithChildren & {
+  loadUser(): Promise<AnyAction>;
+  onClickLogout(): Promise<AnyAction>;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    loadUser: () => fetchCurrentUserThunk()(dispatch),
+    onClickLogout: () => doLogoutThunk()(dispatch),
+  };
+};
+
+const SharedLayoutInner: React.FC<Props> = ({ onClickLogout, loadUser }) => {
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <AppBarMenu>
-      <Outlet />
-    </AppBarMenu>
+    <>
+      <AppBarMenu onClickLogout={onClickLogout}>
+        <Outlet />
+      </AppBarMenu>
+    </>
   );
 };
+
+export const SharedLayout = connect(
+  null,
+  mapDispatchToProps
+)(SharedLayoutInner);
