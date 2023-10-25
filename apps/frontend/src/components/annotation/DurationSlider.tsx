9646d2a15c514471b5987b58375252d24be72185
Younes <younes.benaomar@gmail.com>
2023-10-25 13:24:07 +0100

    add step button to allow easy annotation duration (#218)

--- apps/frontend/src/components/annotation/DurationSlider.tsx
+++ apps/frontend/src/components/annotation/DurationSlider.tsx
@@ -1,8 +1,8 @@
-import { styled } from "@mui/material";
-import Slider, {
-  SliderThumb,
-  SliderValueLabelProps,
-} from "@mui/material/Slider";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { Grid, IconButton, Stack, styled } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import Slider, { SliderValueLabelProps } from "@mui/material/Slider";
 import Tooltip, { tooltipClasses, TooltipProps } from "@mui/material/Tooltip";
 import * as React from "react";
 
@@ -15,7 +15,7 @@ type DurationSliderProps = {
   onChange: (start: number, stop: number) => void;
 };
 
-const minDistance = 60;
+const minDistance = 5;
 
 const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} arrow classes={{ popper: className }} />
@@ -24,7 +24,7 @@ const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
     color: theme.palette.common.white,
   },
   [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: "transparent",
+    backgroundColor: grey[900],
     borderWidth: 1,
     borderColor: theme.palette.common.white,
     borderStyle: "solid",
@@ -50,7 +50,7 @@ export const DurationSlider: React.FC<DurationSliderProps> = ({
   const [value, setValue] = React.useState<number[]>([startTime, stopTime]);
 
   const handleChange = (
-    event: Event,
+    _event: Event | React.MouseEvent,
     newValue: number | number[],
     activeThumb: number
   ) => {
@@ -73,23 +73,63 @@ export const DurationSlider: React.FC<DurationSliderProps> = ({
   };
 
   return (
-    <>
-      <Slider
-        getAriaLabel={() => "Annotation slider"}
-        value={value}
-        onChange={handleChange}
-        valueLabelFormat={formatDuration}
-        step={60}
-        size="small"
-        valueLabelDisplay="on"
-        min={0}
-        max={duration}
-        color="secondary"
-        disableSwap
-        slots={{
-          valueLabel: ValueLabelComponent,
-        }}
-      />
-    </>
+    <Grid container spacing={2} alignItems="center">
+      <Stack direction={"row"}>
+        <IconButton
+          size="small"
+          color="secondary"
+          sx={{ p: 0 }}
+          onClick={(e) => handleChange(e, [value[0] - 1, value[1]], 0)}
+        >
+          <ArrowLeftIcon />
+        </IconButton>
+        <IconButton
+          size="small"
+          color="secondary"
+          sx={{ p: 0 }}
+          onClick={(e) => handleChange(e, [value[0] + 1, value[1]], 0)}
+        >
+          <ArrowRightIcon />
+        </IconButton>
+      </Stack>
+
+      <Grid item xs>
+        <Slider
+          getAriaLabel={() => "Annotation slider"}
+          value={value}
+          onChange={handleChange}
+          valueLabelFormat={formatDuration}
+          step={1}
+          size="small"
+          valueLabelDisplay="on"
+          min={0}
+          max={duration}
+          color="secondary"
+          disableSwap
+          slots={{
+            valueLabel: ValueLabelComponent,
+          }}
+          sx={{ mb: 1 }}
+        />
+      </Grid>
+      <Stack direction={"row"}>
+        <IconButton
+          size="small"
+          color="secondary"
+          sx={{ p: 0 }}
+          onClick={(e) => handleChange(e, [value[0], value[1] - 1], 1)}
+        >
+          <ArrowLeftIcon />
+        </IconButton>
+        <IconButton
+          size="small"
+          color="secondary"
+          sx={{ p: 0 }}
+          onClick={(e) => handleChange(e, [value[0], value[1] + 1], 1)}
+        >
+          <ArrowRightIcon />
+        </IconButton>
+      </Stack>
+    </Grid>
   );
 };
