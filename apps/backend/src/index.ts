d2bb7339e07d746c763e128a4eb2a53ea7b52128
Younes <younes.benaomar@gmail.com>
2023-10-30 17:10:52 +0100

    add log to test backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -23,7 +23,7 @@ async function main() {
 
   // Setup CORS
   app.use(cors({
-    origin: process.env.NODE_ENV != "production" ? ['http://localhost:3000', 'http://localhost:4000'] : undefined,
+    origin: process.env.NODE_ENV != "production" ? ['http://localhost:3000', 'http://localhost:4000', 'http://127.0.0.1:3000', "http://127.0.0.1:4000"] : undefined,
     credentials: true,
   }));
 
