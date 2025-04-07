7ea8635abb8933bebf0b295f99ee055127443a97
Achraf <achraf.benaomar@gmail.com>
2025-04-07 17:38:53 +0100

    chore: fix style project

--- apps/web/src/app/(app)/(auth)/forgot/page.tsx
+++ apps/web/src/app/(app)/(auth)/forgot/page.tsx
@@ -1,12 +1,10 @@
 import { DialogTitle, Paper } from "@mui/material";
-import { useTranslations } from "next-intl";
 import { ForgotForm } from "@/components/auth/forgot-form";
 
 export default function ForgotPage() {
-  const t = useTranslations();
-  return (
-    <Paper>
-      <ForgotForm />
-    </Paper>
-  );
+	return (
+		<Paper>
+			<ForgotForm />
+		</Paper>
+	);
 }
--- apps/web/src/components/project/edit/edit-dialog.tsx
+++ apps/web/src/components/project/edit/edit-dialog.tsx
@@ -7,19 +7,19 @@ import { EditProjectForm } from "./edit-project-form";
 import { EditProjectDialogSkeleton } from "./edit-dialog-skeleton";
 
 export function EditProjectDialog({ projectId }: { projectId: string }) {
-  const t = useTranslations();
-  const router = useRouter();
+	const t = useTranslations();
+	const router = useRouter();
 
-  return (
-    <StyledDialog
-      title={t("project.edit.dialog.title")}
-      onClose={() => router.back()}
-      maxWidth="md"
-      open={true}
-    >
-      <Suspense fallback={<EditProjectDialogSkeleton />}>
-        <EditProjectForm projectId={projectId} />
-      </Suspense>
-    </StyledDialog>
-  );
+	return (
+		<StyledDialog
+			title={t("project.edit.dialog.title")}
+			onClose={() => router.back()}
+			maxWidth="md"
+			open={true}
+		>
+			<Suspense fallback={<EditProjectDialogSkeleton />}>
+				<EditProjectForm projectId={projectId} />
+			</Suspense>
+		</StyledDialog>
+	);
 }
