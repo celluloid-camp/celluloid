a3625593bb0765a6ec5144a39fc3dcace9328219
Younes <younes.benaomar@gmail.com>
2025-04-19 13:07:49 +0100

    chore: fix migration and update tests

--- tests/01-signup.spec.ts
+++ tests/01-signup.spec.ts
@@ -4,64 +4,67 @@ const randomNum = Math.floor(Math.random() * 10000);
 const TEST_USERNANE = `test${randomNum}`;
 const TEST_USER_EMAIL = `${TEST_USERNANE}@server.com`;
 
-let page: Page;
-test.describe.configure({ mode: "serial" });
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-});
+test.describe("signup", () => {
 
-test.afterAll(async () => {
-  await page.close();
-});
 
-test("test user signup", async () => {
-  await page.goto("http://localhost:3000/");
-  await page.getByTestId("header-login-button").click();
-  await page.getByTestId("signup").click();
-  await expect(page).toHaveURL(/.*\/signup/);
+  // test.beforeAll(async (page) => {
 
-  await page.getByTestId("username").click();
-  await page.getByTestId("username").fill(TEST_USERNANE);
-  await page.getByTestId("email").click();
-  await page.getByTestId("email").fill(TEST_USER_EMAIL);
-  await page.getByTestId("password").click();
-  await page.getByTestId("password").fill("testtest");
-  await page.getByTestId("passwordConfirmation").click();
-  await page.getByTestId("passwordConfirmation").fill("testtest");
+  // });
 
-  await page.getByTestId("submit").click();
+  test.afterAll(async ({ page }) => {
+    await page.close();
+  });
 
-  await expect(page).toHaveURL(/.*\/confirm/);
 
-  await page.getByTestId("code").click();
-  await page.getByTestId("code").fill("0000");
-  await page.getByTestId("submit").click();
+  test("test user signup", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await page.getByTestId("header-login-button").click();
+    await page.getByTestId("signup").click();
+    await expect(page).toHaveURL(/.*\/signup/);
 
-  await expect(page).toHaveURL("http://localhost:3000/");
+    await page.getByTestId("username").click();
+    await page.getByTestId("username").fill(TEST_USERNANE);
+    await page.getByTestId("email").click();
+    await page.getByTestId("email").fill(TEST_USER_EMAIL);
+    await page.getByTestId("password").click();
+    await page.getByTestId("password").fill("testtest");
+    await page.getByTestId("passwordConfirmation").click();
+    await page.getByTestId("passwordConfirmation").fill("testtest");
 
-  await page.getByTestId("header-account-menu").click();
-  await page.getByTestId("header-profile-button").click();
+    await page.getByTestId("submit").click();
 
-  await expect(page.getByTestId("profile-header-title")).toHaveText(
-    TEST_USERNANE,
-  );
-});
+    await expect(page).toHaveURL(/.*\/confirm/);
 
-test("test reconnect with existing account without confirmation", async () => {
-  await page.reload();
-  await page.goto("http://localhost:3000/login");
+    await page.getByTestId("code").click();
+    await page.getByTestId("code").fill("0000");
+    await page.getByTestId("submit").click();
 
-  await page.getByTestId("username").click();
-  await page.getByTestId("username").fill(TEST_USER_EMAIL);
-  await page.getByTestId("username").press("Tab");
-  await page.getByTestId("password").fill("testtest");
-  await page.getByTestId("submit").click();
+    await expect(page).toHaveURL("http://localhost:3000/");
 
-  await expect(page).toHaveURL("http://localhost:3000/profile");
+    await page.getByTestId("header-account-menu").click();
+    await page.getByTestId("header-profile-button").click();
 
-  await page.getByTestId("header-account-menu").click();
-  await page.getByTestId("header-logout-button").click();
+    await expect(page.getByTestId("profile-header-title")).toHaveText(
+      TEST_USERNANE,
+    );
+  });
 
-  await expect(page).toHaveURL("http://localhost:3000/");
+  test("test reconnect with existing account without confirmation", async ({ page }) => {
+    await page.reload();
+    await page.goto("http://localhost:3000/login");
+
+    await page.getByTestId("username").click();
+    await page.getByTestId("username").fill(TEST_USER_EMAIL);
+    await page.getByTestId("username").press("Tab");
+    await page.getByTestId("password").fill("testtest");
+    await page.getByTestId("submit").click();
+
+    await expect(page).toHaveURL("http://localhost:3000/profile");
+
+    await page.getByTestId("header-account-menu").click();
+    await page.getByTestId("header-logout-button").click();
+
+    await expect(page).toHaveURL("http://localhost:3000/");
+  });
 });
