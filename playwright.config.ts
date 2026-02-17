da1f5074d0b3094f83de1ff81955330c4f28415c
Younes <younes.benaomar@gmail.com>
2026-02-17 10:54:20 +0000

    chore: Remove webServer configuration from Playwright setup to streamline test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,14 +74,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: [
-    {
-      command: "bun run web start",
-      url: "http://localhost:3000",
-      reuseExistingServer: true,
-      timeout: 120 * 1000,
-    },
-  ],
 });
